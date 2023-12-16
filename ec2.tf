4d48989d3d9182159c2e324644ee886d7f62bae9
unknown <caveaku>
2023-12-16 16:51:25 -0500

    updated repo

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@
 
 resource "aws_instance" "web" {
   ami           = "ami-08df646e18b182346"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   key_name = "pswain"
   subnet_id = aws_subnet.public[count.index].id
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
